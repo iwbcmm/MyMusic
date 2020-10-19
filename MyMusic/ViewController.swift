395e4af373344dbd0961059000e1e66f886c89b1
岩渕真未 <mami.iwabuchi@talkoot.co.jp>
2020-10-19 12:49:02 +0900

    p.164まで

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by 岩渕真未 on 2020/10/19.
+//  Copyright © 2020 岩渕真未. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    var cymbalPlayer = AVAudioPlayer()
+    
+    @IBAction func cymbal(_ sender: Any) {
+        do {
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            
+            cymbalPlayer.play()
+        } catch {
+            print("シンバルで、エラーが発生しました！")
+        }
+        
+    }
+    
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    
+    var guitarPlayer = AVAudioPlayer()
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            guitarPlayer.play()
+        } catch {
+            
+            print("ギターで、エラーが発生しました！")
+        }
+    }
+    
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    var backmusicPlayer = AVAudioPlayer()
+    
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            
+            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
+            
+            backmusicPlayer.numberOfLoops = -1
+            backmusicPlayer.play()
+        } catch {
+            
+            print("エラーが発生しました！")
+        }
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
+    
+    
+}
+
