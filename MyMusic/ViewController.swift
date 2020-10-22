933c9ac7c260f6931d9433be89f3279a63038c11
岩渕真未 <mami.iwabuchi@talkoot.co.jp>
2020-10-22 11:09:20 +0900

    リファクタリングした

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -21,13 +21,7 @@ class ViewController: UIViewController {
     var cymbalPlayer = AVAudioPlayer()
     
     @IBAction func cymbal(_ sender: Any) {
-        do {
-            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
-            
-            cymbalPlayer.play()
-        } catch {
-            print("シンバルで、エラーが発生しました！")
-        }
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
         
     }
     
@@ -36,14 +30,7 @@ class ViewController: UIViewController {
     var guitarPlayer = AVAudioPlayer()
     
     @IBAction func guitar(_ sender: Any) {
-        do {
-            
-            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-            guitarPlayer.play()
-        } catch {
-            
-            print("ギターで、エラーが発生しました！")
-        }
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
     }
     
     let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
@@ -52,22 +39,22 @@ class ViewController: UIViewController {
     
     
     @IBAction func play(_ sender: Any) {
-        do {
-            
-            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
-            
-            backmusicPlayer.numberOfLoops = -1
-            backmusicPlayer.play()
-        } catch {
-            
-            print("エラーが発生しました！")
-        }
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
     }
     
     @IBAction func stop(_ sender: Any) {
         backmusicPlayer.stop()
     }
-    
+ 
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int){
+        do{
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました！")
+        }
+    }
     
 }
 
